9ae3336bf62d8340c3d1616e3d5f6f4180d5d1c3
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-08-08 21:29:50 +0545

    setup routes

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,13 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const connection = require("./db/connection");
+connection();
+
+app.use(express.json()); //body parser
+
+const userRoute = require("./routes/user");
+app.use(userRoute);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
---
+++ server/src/routes/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { registerNewUser, loginUser } = require("../controllers/user");
+
+router.post("/register", registerNewUser);
+router.post("/login", loginUser);
+
+module.exports = router;
